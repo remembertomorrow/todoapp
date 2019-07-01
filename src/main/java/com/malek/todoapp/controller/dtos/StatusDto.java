c968c60dd289722765ffd9da341f6b74d2f4b8a2
Malek <s14114@pjwstk.edu.pl>
2019-07-01 20:05:40 +0100

    Status and Task DTOs added

--- src/main/java/com/malek/todoapp/controller/dtos/StatusDto.java
+++ src/main/java/com/malek/todoapp/controller/dtos/StatusDto.java
@@ -1,4 +1,20 @@
 package com.malek.todoapp.controller.dtos;
 
+import com.malek.todoapp.model.StatusEnum;
+import com.malek.todoapp.model.Task;
+import java.time.LocalDateTime;
+import java.util.Set;
+
 public class StatusDto {
+
+    private Long statusId;
+
+    private StatusEnum status;
+
+    private LocalDateTime startDate;
+
+    private LocalDateTime finishDate;
+
+    private Set<Task> products;
+
 }
--- src/main/java/com/malek/todoapp/controller/dtos/TaskDto.java
+++ src/main/java/com/malek/todoapp/controller/dtos/TaskDto.java
@@ -1,4 +1,22 @@
 package com.malek.todoapp.controller.dtos;
 
+import com.malek.todoapp.model.Status;
+
+import java.time.LocalDateTime;
+
 public class TaskDto {
+
+    private Long taskId;
+
+    private String title;
+
+    private String description;
+
+    private LocalDateTime dueDate;
+
+    private LocalDateTime createDate;
+
+    private LocalDateTime modifyDate;
+
+    private Status status;
 }
